c4b524e85c127154e8f2d331b1c888df02c91c68
i333656 <vasil.stoyanov@feg.bg>
2018-09-22 13:45:40 +0200

    change room class and add room service endpoints

--- room/src/main/java/service/models/Room.java
+++ room/src/main/java/service/models/Room.java
@@ -1,10 +1,13 @@
 package service.models;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public class Room {
 
+    private String id;
     private String address;
+    private String city;
     private String landlord;
     private int rent;
     private String status;
@@ -12,12 +15,18 @@ public class Room {
     public Room() {
     }
 
-    public Room(String address, String landlord, int rent, String status) {
+    public Room(String address, String city, String landlord, int rent) {
+        this.id = UUID.randomUUID().toString();
         this.address = address;
+        this.city = city;
         this.landlord = landlord;
         this.rent = rent;
-        this.status = status;
+        this.status = "free";
     }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
 
     public String getAddress() {
         return address;
@@ -27,6 +36,10 @@ public class Room {
         this.address = address;
     }
 
+    public String getCity() { return city; }
+
+    public void setCity(String city) { this.city = city; }
+
     public String getLandlord() {
         return landlord;
     }
@@ -35,9 +48,7 @@ public class Room {
         this.landlord = landlord;
     }
 
-    public int getRent() {
-        return rent;
-    }
+    public int getRent() { return rent; }
 
     public void setRent(int rent) {
         this.rent = rent;
@@ -56,7 +67,7 @@ public class Room {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Room user = (Room) o;
-        return this.address.equals(user.address);
+        return this.id.equals(user.id);
     }
 
     @Override
@@ -65,8 +76,18 @@ public class Room {
     @Override
     public String toString() {
         return "model.Room{address='" + this.address +
+                "',city='" + this.city +
                 "',landlord='" + this.landlord +
                 "',rent='" + this.rent +
+                "',id='" + this.id+
                 "',status='" + this.status + "'}";
+    }
+
+    public void book() {
+        setStatus("booked");
+    }
+
+    public void rent() {
+        setStatus("rented");
     }
 }
